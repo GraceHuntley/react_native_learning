1ef63fc4b83087123e56a050b9b8e4af1da90100
ColMac <cmaccannell@gmail.com>
2019-04-30 10:15:10 -0700

    second commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+const component = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
+
+export default component;
+
+//ReactNative.AppRegistry.registerComponent('albums', () => App);
